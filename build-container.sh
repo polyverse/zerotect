6263c044e7550aa31de9646747acaec28704a0eb
Archis Gore <archis@polyverse.com>
2020-09-14 13:05:29 -0700

    Use polyverse's build container which has clippy

--- build-container.sh
+++ build-container.sh
@@ -2,4 +2,4 @@
 
 #docker run --rm -it -v $PWD:/zerotect --privileged rust bash
 
-docker run -v cargo-cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged clux/muslrust /bin/bash -c "rustup component add clippy && cargo install cargo-bloat && bash" 
+docker run -v cargo-cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged polyverse/rust-dev-env
