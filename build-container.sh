fd95bb31d934c5b9ec83e121b63d988a669500e7
Archis <archis@polyverse.com>
2021-07-14 01:19:37 -0700

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-IMAGE="ghcr.io/polyverse/rust-dev-env/rust-dev-env"
+IMAGE="ghcr.io/polyverse/rust-dev-env"
 
 docker run -v /Users/archisgore/.rust_carco_cache:/root/.cargo/registry -v $PWD:/volume --rm -it --privileged $IMAGE
